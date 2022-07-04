33def0bd1b6b8802f8927d344fdf66cf30b187c6
Lee-DoHa <johnny550@naver.com>
2022-07-04 16:20:05 +0900

    post 기능 추가

---
+++ src/main/java/com/dohalog/controller/PostController.java
@@ -0,0 +1,33 @@
+package com.dohalog.controller;
+
+import com.dohalog.request.PostCreate;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Slf4j
+@RestController
+public class PostController {
+
+    @PostMapping("/posts")
+    public Map<String, String> post(@RequestBody @Valid PostCreate params, BindingResult result)throws Exception{
+        if (result.hasErrors()){
+            List<FieldError> fieldErrors = result.getFieldErrors();
+            FieldError firstFieldError = fieldErrors.get(0);
+            String fieldName = firstFieldError.getField();
+            String errorMessage = firstFieldError.getDefaultMessage();
+
+            Map<String, String > error = new HashMap<>();
+            error.put(fieldName, errorMessage);
+            return error;
+        }
+        return Map.of();
+    }
+
+}
