a115bf248d5cfeda3583fb8921a267bc711ddf29
Lee-DoHa <johnny550@naver.com>
2022-07-04 16:15:31 +0900

    spring 공부용 세팅

---
+++ src/main/java/com/dohalog/DohalogApplication.java
@@ -0,0 +1,13 @@
+package com.dohalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DohalogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DohalogApplication.class, args);
+    }
+
+}
