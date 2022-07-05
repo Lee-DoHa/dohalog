df380ff0594ecf35d8559831cfbd90a8b202df22
Lee-DoHa <johnny550@naver.com>
2022-07-05 15:16:40 +0900

    Update Controller

--- src/main/java/com/dohalog/controller/PostController.java
+++ src/main/java/com/dohalog/controller/PostController.java
@@ -1,6 +1,8 @@
 package com.dohalog.controller;
 
 import com.dohalog.request.PostCreate;
+import com.dohalog.service.PostService;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
@@ -13,10 +15,16 @@ import java.util.Map;
 
 @Slf4j
 @RestController
+@RequiredArgsConstructor
 public class PostController {
 
+    private final PostService postService;
+
     @PostMapping("/posts")
-    public Map<String, String> post(@RequestBody @Valid PostCreate params){
+    public Map<String, String> post(@RequestBody @Valid PostCreate request){
+        // repository.save(params)
+        // db.save(params)
+        postService.write(request);
         return Map.of();
     }
 
