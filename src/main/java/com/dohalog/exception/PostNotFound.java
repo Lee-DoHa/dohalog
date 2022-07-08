d950a9e62588cad552e593ffdbf1ba29bb541d89
Lee-DoHa <johnny550@naver.com>
2022-07-08 11:19:35 +0900

    Update PostNotFound.java

--- src/main/java/com/dohalog/exception/PostNotFound.java
+++ src/main/java/com/dohalog/exception/PostNotFound.java
@@ -1,6 +1,8 @@
 package com.dohalog.exception;
-
-public class PostNotFound extends RuntimeException{
+/**
+ * status -> 404
+ */
+public class PostNotFound extends DohalogException{
 
     private static final String MESSAGE = "존재하지 않는 글입니다.";
 
@@ -8,7 +10,9 @@ public class PostNotFound extends RuntimeException{
         super(MESSAGE);
     }
 
-//    public PostNotFound(Throwable cause) {
-//        super(MESSAGE, cause);
-//    }
+
+    @Override
+    public int getStatusCode() {
+        return 404;
+    }
 }
