e5c1743486b06e7d1461a2b7920df9a610d6b1e4
Lee-DoHa <johnny550@naver.com>
2022-07-08 11:18:56 +0900

    Update ExceptionController.java

--- src/main/java/com/dohalog/controller/ExceptionController.java
+++ src/main/java/com/dohalog/controller/ExceptionController.java
@@ -1,17 +1,17 @@
 package com.dohalog.controller;
 
+import com.dohalog.exception.DohalogException;
+import com.dohalog.exception.InvalidRequest;
 import com.dohalog.response.ErrorResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
-
-import java.util.HashMap;
-import java.util.Map;
 
 @Slf4j
 @ControllerAdvice
@@ -32,4 +32,28 @@ public class ExceptionController {
 
         return response;
     }
+
+    @ResponseBody
+    @ExceptionHandler(DohalogException.class)
+    public ResponseEntity<ErrorResponse> dohalogException(DohalogException e) {
+        int statusCode = e.getStatusCode();
+
+        ErrorResponse body = ErrorResponse.builder()
+                .code(String.valueOf(statusCode))
+                .message(e.getMessage())
+                .build();
+
+        if (e instanceof InvalidRequest) {
+            InvalidRequest invalidRequest = (InvalidRequest) e;
+            String fieldName = invalidRequest.getFieldName();
+            String message = invalidRequest.getMessage();
+            body.addValidation(fieldName, message);
+        }
+
+        ResponseEntity<ErrorResponse> response = ResponseEntity.status(statusCode)
+                .body(body);
+
+        return response;
+    }
+
 }
