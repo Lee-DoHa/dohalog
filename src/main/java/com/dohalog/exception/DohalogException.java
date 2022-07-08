30d9b1797c6707fe74aa8cebc5572960979a9845
Lee-DoHa <johnny550@naver.com>
2022-07-08 11:43:43 +0900

    Update DohalogException.java

--- src/main/java/com/dohalog/exception/DohalogException.java
+++ src/main/java/com/dohalog/exception/DohalogException.java
@@ -1,7 +1,14 @@
 package com.dohalog.exception;
 
+import lombok.Getter;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Getter
 public abstract class DohalogException extends RuntimeException{
 
+    public final Map<String, String> validation = new HashMap<>();
 
     public DohalogException(String message) {
         super(message);
@@ -12,4 +19,8 @@ public abstract class DohalogException extends RuntimeException{
     }
 
     public abstract int getStatusCode();
+
+    public void addValidation(String fieldName, String message) {
+        validation.put(fieldName, message);
+    }
 }
