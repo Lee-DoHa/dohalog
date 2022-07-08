6537f58e1221f719bbf887d1e92f07d199977f6b
Lee-DoHa <johnny550@naver.com>
2022-07-08 11:19:19 +0900

    Update PostControllerTest.java

--- src/test/java/com/dohalog/controller/PostControllerTest.java
+++ src/test/java/com/dohalog/controller/PostControllerTest.java
@@ -207,6 +207,54 @@ class PostControllerTest {
                 .andExpect(status().isOk())
                 .andDo(print());
     }
+    @Test
+    @DisplayName("존재하지 않는 게시글 조회")
+    void test8() throws Exception {
+
+        // expected
+        mockMvc.perform(get("/posts/{postId}", 1L)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isNotFound())
+                .andDo(print());
+    }
+
+    @Test
+    @DisplayName("존재하지 않는 게시글 수정")
+    void test9() throws Exception {
+        // given
+        Post postEdit = Post.builder()
+                .title("도하씨")
+                .content("굳잡뿡빵")
+                .build();
+
+        // expected
+        mockMvc.perform(MockMvcRequestBuilders.patch("/posts/{postId}", 1L)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(postEdit)))
+                .andExpect(status().isNotFound())
+                .andDo(print());
+    }
+
+    @Test
+    @DisplayName("/게시글 작성시 제목에 '바보'는 포함될 수 없다.")
+    void test10() throws Exception{
+        // given
+        PostCreate request = PostCreate.builder()
+                .title("나는 바보입니다..")
+                .content("뿡뿡뿡내용.")
+                .build();
+
+        String json = objectMapper.writeValueAsString(request);// String -> JSON 으로 바꿔줌
+
+        // when (이런 요청을 했을 때)
+        mockMvc.perform(post("/posts")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(json)
+                )
+                .andExpect(status().isBadRequest())
+                .andDo(print());
+
+    }
 
 }
 
