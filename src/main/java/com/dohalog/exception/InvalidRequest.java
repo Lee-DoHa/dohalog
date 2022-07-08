16a943f8d7072dd0c0cd8da1e337eeef9689a38b
Lee-DoHa <johnny550@naver.com>
2022-07-08 11:19:04 +0900

    Update InvalidRequest.java

---
+++ src/main/java/com/dohalog/exception/InvalidRequest.java
@@ -0,0 +1,30 @@
+package com.dohalog.exception;
+
+import lombok.Getter;
+
+/**
+ * status -> 400
+ */
+@Getter
+public class InvalidRequest extends DohalogException{
+
+    private static final String MESSAGE = "잘못된 요청입니다.";
+
+    public String fieldName;
+    public String message;
+
+    public InvalidRequest() {
+        super(MESSAGE);
+    }
+
+    public InvalidRequest(String fieldName, String message) {
+        super(MESSAGE);
+        this.fieldName = fieldName;
+        this.message = message;
+    }
+
+    @Override
+    public int getStatusCode() {
+        return 400;
+    }
+}
